6c33bd7c44dcc7ef2e55e946f409c48c137044b1
Glascode <mael.querre@gmail.com>
2018-06-23 02:42:56 +0200

    Rework design

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(".loop-items").each(function (loop) {
+        loop += 1;
+        $(this).find(".loop-item").each(function (step) {
+
+            /* Adjust step number */
+            step += 1;
+            if (step < 10) {
+                step = "0" + step;
+            }
+
+            /* Image */
+            var src = "images/l" + loop + "s" + step + ".jpg";
+            $link = $(this).find("a");
+            $link.append("<img class='loop-image' src='" + src + "'>");
+        });
+    });
+});
